d1585f2e6931c8524673b9f1f915774563c02c3c
Timo <t.reichelt@live.de>
2022-01-13 09:49:08 +0800

    Included CORS middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
@@ -14,5 +14,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodGet, "/v1/instruction/:id", app.getOneInstruction)
 	router.HandlerFunc(http.MethodGet, "/v1/instructions", app.getAllInstructions)
 
-	return router
+	return app.enableCORS(router)
 }
